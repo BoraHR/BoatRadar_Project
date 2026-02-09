dfdb045bde0bdba1a212011a039f3312418c009a
BoraHR <1066706@hr.nl>
2026-02-09 15:01:38 +0100

    - added Spectre.Console to better simulate a radar - colored the radar frame. - V2 folder wit new project added to experiment with Spectre.Console, will likely use V1 however.

---
+++ V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Spectre.Console;
+using System.Timers;
+
+Random rand = new Random();
+var screen = new Canvas(80, 40);
+var timer = new System.Timers.Timer(100);
+
+int width = 80;
+int height = 40;
+
+// vessel positions
+double x = 40, y = 20;
+double vx = 0.5, vy = 0.2;
+
+double x2 = 10, y2 = 10;
+double vx2 = 0.3, vy2 = 0.4;
+
+timer.Elapsed += (s, e) =>
+{
+    AnsiConsole.Clear();
+
+    // update positions
+    x += vx;
+    y += vy;
+    x2 += vx2;
+    y2 += vy2;
+
+    // bounce off walls
+    if (x <= 0 || x >= width - 1) vx *= -1;
+    if (y <= 0 || y >= height - 1) vy *= -1;
+
+    if (x2 <= 0 || x2 >= width - 1) vx2 *= -1;
+    if (y2 <= 0 || y2 >= height - 1) vy2 *= -1;
+
+    // draw
+    screen.SetPixel((int)x, (int)y, Color.Green);
+    screen.SetPixel((int)x2, (int)y2, Color.Red);
+
+    AnsiConsole.Clear();
+    AnsiConsole.Write(screen);
+};
+
+timer.Start();
+Console.ReadKey();
