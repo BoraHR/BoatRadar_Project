b4b4d678941d4750577ddfcd4cbfa7f83d002421
BoraHR <1066706@hr.nl>
2026-02-23 15:13:36 +0100

    - Invalid data are no longer inserted in database. - better console information in the helper function.

--- AIS_Response_Manager/pyais_decoder.py
+++ AIS_Response_Manager/pyais_decoder.py
@@ -3,11 +3,14 @@ import sqlite3
 from pyais import decode
 from pyais.stream import FileReaderStream
 import time
+from datetime import datetime, timedelta
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ais_file1 = os.path.join(BASE_DIR, "Data/ais_arca.txt")
 ais_file2 = os.path.join(BASE_DIR, "Data/ais_rp42.txt")
 FileRoute_sql3 = os.path.join(BASE_DIR, "Data/AIS-Responder_DB.db")
+
+import math
 
 def OpenDB():
         NotImplementedError
@@ -29,17 +32,19 @@ def Decode_file(file):
 
                 try:
                     decoded = decode(nmea_sentence)
-                    print("Decoded message:")
-                    print(decoded)
-                    print("-" * 50)
-                    end = time.time()
-                    print(f"ms: {end - start}")
+                    if(181.0 == float(decoded.lon) or 91.0 == float(decoded.lat) or 0.00 > float(decoded.lon) or 0.00 > float(decoded.lat)):
+                        print("Decoded message:")
+                        print(decoded)
+                        print("-" * 50)
+                        end = time.time()
+                        print(f"ms: {end - start}")
                 except Exception as e:
-                    print("Failed to decode:", nmea_sentence)
-                    print("Error:", e)
-                    print("-" * 50)
-                    end = time.time()
-                    print(f"ms: {end - start}")
+                    # print("Failed to decode:", nmea_sentence)
+                    # print("Error:", e)
+                    # print("-" * 50)
+                    # end = time.time()
+                    # print(f"ms: {end - start}")
+                    pass
 
 def Save_DecodedData(_file):
     total_time = 0.00
@@ -53,11 +58,12 @@ def Save_DecodedData(_file):
     c.execute('''
         CREATE TABLE IF NOT EXISTS AIS_Decoder (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            MsgType TEXT,
-            Repeat TEXT,
+            Date TEXT,
+            MsgType INTEGER,
+            Repeat INTEGER,
             Mmsi TEXT NOT NULL CHECK (length(Mmsi) = 9),
-            Status TEXT,
-            Turn TEXT,
+            Status INTEGER,
+            Turn REAL,
             Speed REAL,
             Accuracy INTEGER,
             Longitude REAL,
@@ -84,18 +90,20 @@ def Save_DecodedData(_file):
 
             # Extract only the AIS sentence (starts with $AIVDM)
             if "$AIVDM" in line:
-                nmea_sentence = line.split("$AIVDM")[-1]
-                nmea_sentence = "$AIVDM" + nmea_sentence
+                frag_sentence = line.split("$AIVDM")
+                nmea_sentence = "$AIVDM" + frag_sentence[1]
+                date = frag_sentence[0].split(" ")[1]
                 try:
                     decoded = decode(nmea_sentence)
                     accuracyInt = int(bool(decoded.accuracy))
                     raimInt = int(bool(decoded.raim))
                     if(decoded.mmsi and len(str(decoded.mmsi).strip()) == 9):
-                        if(ValidateFields(decoded) == True):
+                        if(ValidateFields(decoded) == True and ValidateDate(date) == True):
                             try:
                                 c.execute('''
                                     INSERT INTO AIS_Decoder
                                     (
+                                        Date,
                                         MsgType, 
                                         Repeat,
                                         Mmsi,
@@ -113,9 +121,10 @@ def Save_DecodedData(_file):
                                         Raim,
                                         Radio
                                     )
-                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                                 ''',
                                 (
+                                    date,
                                     decoded.msg_type,
                                     decoded.repeat,
                                     decoded.mmsi,
@@ -136,6 +145,7 @@ def Save_DecodedData(_file):
                             )
                                 end = time.time()
                                 print("Passed:")
+                                print(date)
                                 print(nmea_sentence)
                                 print(decoded)
                                 print(f"ms: {(end - start) * 1000:.2f}")
@@ -144,6 +154,7 @@ def Save_DecodedData(_file):
                                 passed += 1
                             except Exception as e:
                                 print("Failed:")
+                                print(date)
                                 print(nmea_sentence)
                                 print(decoded)
                                 print("Do to an error during SQL Insertion")
@@ -155,6 +166,7 @@ def Save_DecodedData(_file):
                                 failed += 1
                         else:
                             print("Skipped:")
+                            print(date)
                             print(nmea_sentence)
                             print(decoded)
                             print("Because field validation failed")
@@ -166,9 +178,10 @@ def Save_DecodedData(_file):
                             continue
                     else:
                         print("Failed:")
+                        print(date)
                         print(nmea_sentence)
                         print(decoded)
-                        print("Because it has an invalid mssi")
+                        print("Because one or more vailidation have failed")
                         end = time.time()
                         print(f"ms: {(end - start) * 1000:.2f}")
                         print("-" * 50)
@@ -176,7 +189,7 @@ def Save_DecodedData(_file):
                         failed += 1
                         continue
                 except Exception as e:
-                    print("Failed to decode:", nmea_sentence)
+                    print("Failed to decode:", date, nmea_sentence)
                     print("Error:", e)
                     end = time.time()
                     print(f"ms: {(end - start) * 1000:.2f}")
@@ -190,7 +203,8 @@ def Save_DecodedData(_file):
 
 def ValidateFields(decoded) -> bool:
     try:
-        if(decoded.msg_type is None or 
+        if(
+            decoded.msg_type is None or 
             decoded.repeat is None or 
             decoded.mmsi is None or 
             decoded.status is None or
@@ -208,15 +222,80 @@ def ValidateFields(decoded) -> bool:
         ):
             print("Validation failed one or more fields are None")
             return False
-        else:
-            return True
+        if(
+            decoded.lon == 181.00 or
+            decoded.lat == 91.00 or
+            decoded.course == 360.00 or
+            decoded.heading == 511 or
+            decoded.second == 63
+        ):
+            print("Validation failed one or more fields have invalid data")
+            return False
+        return True
     except Exception as e:
-        print("Validation failed do to exception:")
+        print("Field validation failed do to exception:")
         print(e)
         return False
     
-# 0.009 is 1KM 
-def InRangeHelper(boat_id, range=0.009):
+def ValidateDate(strDate, isDebug = False) -> bool:
+    # https://docs.python.org/3/library/datetime.html
+    try:
+        date = datetime.fromisoformat(strDate)
+        Y = date.year
+        M = date.month
+        D = date.day
+        h = date.hour
+        m = date.minute
+        s = date.second
+        ms = date.microsecond
+        if(isDebug is True):
+            if len(str(D)) == 1:
+                D = "0" + str(D)
+            if len(str(M)) == 1:
+                M = "0" + str(M)
+            if len(str(h)) == 1:
+                h = "0" + str(h)
+            if len(str(m)) == 1:
+                m = "0" + str(m)
+            if len(str(s)) == 1:
+                s = "0" + str(s)
+            print(f"{D}-{M}-{Y} | {h}:{m}:{s}:{ms}")
+            print("Passed validation")
+        return True
+    except Exception as e:
+        print("Date vaidation failed do to exception:")
+        print(e)
+        return False
+    return True
+
+
+# https://www.geeksforgeeks.org/dsa/haversine-formula-to-find-distance-between-two-points-on-a-sphere/
+# ----------------------------------------------------------------------------------------------------
+# Python 3 program for the
+# haversine formula
+def haversine(lat1, lon1, lat2, lon2):
+    
+    # distance between latitudes
+    # and longitudes
+    dLat = (lat2 - lat1) * math.pi / 180.0
+    dLon = (lon2 - lon1) * math.pi / 180.0
+
+    # convert to radians
+    lat1 = (lat1) * math.pi / 180.0
+    lat2 = (lat2) * math.pi / 180.0
+
+    # apply formulae
+    a = (pow(math.sin(dLat / 2), 2) + 
+         pow(math.sin(dLon / 2), 2) * 
+             math.cos(lat1) * math.cos(lat2));
+    rad = 6371
+    c = 2 * math.asin(math.sqrt(a))
+    return rad * c
+
+# range = distance between your boat and other boat in both Lad and long
+# timeWindow = how far appart the received message is allowed to be shown in the list of other Boats
+def InRangeHelper(boat_id, range=0.009, timeWindow = 10.00):
+    dataTuple = "(id, Date, MsgType, Repeat, Mmsi, Status, Turn, Speed, Accuracy, Longitude, Latitude, Course, Heading, Second, Manuever, Spare_1, Raim, Radio)"
     conn = sqlite3.connect(FileRoute_sql3)
     c = conn.cursor()
 
@@ -230,38 +309,60 @@ def InRangeHelper(boat_id, range=0.009):
         print("Boat not found")
         return
     
-    # mmsi = 3
+    # mmsi = 4
     # lon = 9
     # lat = 10
-    mmsi = myBoat[3]
-    lon = myBoat[8]
-    lat = myBoat[9]
-    otherBoats = c.execute('''
+    mmsi = myBoat[4]
+    lon = myBoat[9]
+    lat = myBoat[10]
+    try:
+        dateOfPosition = datetime.fromisoformat(myBoat[1])
+    except Exception as e:
+        print("Failed datetime operation (MyBoat):")
+        print(myBoat)
+        print(e)
+        return
+    otherBoatsNoTime = c.execute('''
         SELECT *
         FROM AIS_Decoder
         WHERE Mmsi != ?
         AND ABS(Longitude - ?) <= ?
         AND ABS(Latitude - ?) <= ?
     ''', 
-    (myBoat[3], lon, range, lat, range)).fetchall()
+    (mmsi, lon, range, lat, range)).fetchall()
+    otherBoats = []
+    for boat in otherBoatsNoTime:
+        try:
+            otherDate = datetime.fromisoformat(boat[1])
+            minDateTime = otherDate + timedelta(seconds=-timeWindow)
+            maxDateTime = otherDate + timedelta(seconds=timeWindow)
+            if (timeWindow <= 0.0):
+                otherBoats.append(boat)
+            elif(dateOfPosition > minDateTime and dateOfPosition < maxDateTime):
+                otherBoats.append(boat)
+        except Exception as e:
+            print("Boat: ", otherBoatsNoTime)
+            print("Failed datetime operation:")
+            print(e)
+            
     print("YourBoat:")
+    print(dataTuple)
     print(myBoat)
+    print(f"lon: {myBoat[9]}, lat: {myBoat[10]}")
     print()
     print(f"Other boats in range of {range} lon and lat:")
+    print(dataTuple)
     for boat in otherBoats:
         print(boat)
+        print(f"lon: {boat[9]}, lat: {boat[10]}")
+        print(f"Distance from myBoat in meters: {haversine(lat, lon, boat[9], boat[10])}")
 
     print()
     conn.close()
 
-InRangeHelper(1)
-# InRangeHelper(1, 9)
-# InRangeHelper(1, 12)
-# InRangeHelper(1, 16)
-# InRangeHelper(1, 30)
 
-
-
-
-
-
+# Save_DecodedData(ais_file1)
+# Save_DecodedData(ais_file2)
+# InRangeHelper(1, 3, 5)
+Decode_file(ais_file1)
+Decode_file(ais_file2)
