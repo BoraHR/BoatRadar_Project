863ba9ed13574341160e037b79ebf38491f9c0d7
BoraHR <1066706@hr.nl>
2026-08-14 13:01:16 +0200

    moved print.

--- AIS_Response_Manager/pyais_decoder.py
+++ AIS_Response_Manager/pyais_decoder.py
@@ -49,8 +49,6 @@ def Decode_file(file):
                     pass
 
 def Save_DecodedData(_file):
-    print(f"Destination: {FileRoute_sql3}")
-    print(f"Destination_test: {test_FileRoute_sql3}")
     total_time = 0.00
     failed = 0
     skipped = 0
@@ -83,6 +81,8 @@ def Save_DecodedData(_file):
     ''') 
     conn.commit()
     print("DB created")
+    print(f"Destination: {FileRoute_sql3}")
+    print(f"Destination_test: {test_FileRoute_sql3}")
     with open(_file, "r", encoding="utf-8", errors="ignore") as file:
         for line in file:
             start = time.time()
